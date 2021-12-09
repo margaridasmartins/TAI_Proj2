7c6390d841a0320fe36b5da088aaff3988847725
leand12 <leandrosilva12@ua.pt>
2021-12-09 12:11:37 +0000

    locatelang
    Co-authored-by: Margarida Martins <margaridasmartins@users.noreply.github.com>
    Co-authored-by: Diogo Mendonça <DoctorTux@users.noreply.github.com>

--- src/lang.hpp
+++ src/lang.hpp
@@ -1,4 +1,16 @@
+#include <dirent.h>
+#include <stdio.h>
+
+#include <limits>
+#include <list>
+#include <string>
+
 #include "fcm.hpp"
+
+struct lang_FCM {
+  FCM *fcm;
+  string lang;
+};
 
 double get_numbits(FCM *fcm, FILE *fptr_t, uint k, float a) {
   double bits = 0;
@@ -24,6 +36,55 @@ double get_numbits(FCM *fcm, FILE *fptr_t, uint k, float a) {
 
   } while (next_char != EOF);
 
+  return (double)bits / l;
+}
 
-  return (double)bits/l;
+void locatelang(list<lang_FCM> lang_list, FILE *fptr, float a, uint k) {
+  double bits = 0;
+  double min_bits = 1000;
+  string current_lang;
+  string last_lang;
+  string lang;
+  uint buffer;
+  char context[k];
+  uint char_count;  
+
+  // first k letters
+  fgets(context, k + 1, fptr);
+  char_count = k + 1;
+
+  // k+1 letter
+  char next_char = fgetc(fptr);
+  do {
+    min_bits = 1000;
+    for (auto v : lang_list) {
+      bits = v.fcm->letter_entropy(context, next_char, a);
+      if (bits < min_bits) {
+        min_bits = bits;
+        lang = v.lang;
+      }
+    }
+    if (lang.compare(last_lang) != 0) {
+          buffer = 0;
+          last_lang=lang;
+    }
+    if (current_lang.compare(lang) != 0) {
+      buffer++;
+      if (buffer == 10) {
+        current_lang = lang;
+        buffer = 0;
+        printf("%s: %d\n", current_lang.c_str(), char_count - buffer);
+      }
+    }
+
+    char_count++;
+    // slide one
+    for (uint i = 0; i < k - 1; i++) {
+      context[i] = context[i + 1];
+    }
+    context[k - 1] = next_char;
+
+    next_char = fgetc(fptr);
+
+  } while (next_char != EOF);
 }
--- src/locatelang.cpp
+++ src/locatelang.cpp
@@ -1,28 +1,33 @@
-#include "lang.hpp"
+#include <dirent.h>
 
-using namespace std;
+#include <list>
+
+#include "fcm.hpp"
+#include "lang.hpp"
 
 int main(int argc, char *argv[]) {
   string help_text =
       "Usage:\n"
-      "  ./lang models_dir filename_t context_size alpha \n"
+      "  ./locatelang models_dir filename_t context_size alpha \n"
       "Required:\n"
-      "  models_dir     The name of the directory with the language models"
+      "  models_dir       The name of the directory with the language files\n"
       "  filename_t       The name of the file with the text under analysis\n"
       "  context_size   The size of the context which translates into the "
       "order of the model\n"
       "  alpha          The value for the smoothing parameter\n"
       "Example:\n"
-      "  ./lang ?? 2 0.5\n";
+      "  ./locatelang ?? ?? 2 0.5\n";
 
-  if (argc < 4) {
+  if (argc < 5) {
     printf("ERR: Incorrect number of arguments\n\n%s", help_text.c_str());
     exit(1);
   }
 
   uint k;
   float a;
+  char models_dir[100];
   char filename_t[100];
+  sprintf(models_dir, "%s", argv[1]);
   sprintf(filename_t, "%s", argv[2]);
   k = atoi(argv[3]);
   a = atof(argv[4]);
@@ -35,43 +40,36 @@ int main(int argc, char *argv[]) {
 
   struct dirent *entry;
   DIR *dp;
-
-  dp = opendir(argv[1]);
+  dp = opendir(models_dir);
   if (dp == NULL) {
     perror("opendir: Path does not exist or could not be read.");
     return -1;
   }
-
   double min_entropy = std::numeric_limits<double>::infinity();
   string lang;
   string s;
+  list<lang_FCM> lang_fcm;
 
   while ((entry = readdir(dp))) {
     if (entry->d_name[0] != '.') {
       FILE *fptr;
       s = entry->d_name;
       char filename[100];
-      sprintf(filename, "%s/%s", argv[1], s.c_str());
+      sprintf(filename, "%s/%s",models_dir, s.c_str());
 
       if ((fptr = fopen(filename, "r")) == NULL) {
         printf("ERR: File \"%s\" not found\n", filename);
         exit(2);
       }
       FCM *fcm = new FCM(k);
-      fcm->train(fptr, 0);
-      double nbits = get_numbits(fcm, fptr_t, k, 0.001);
-      if (nbits < min_entropy) {
-        min_entropy = nbits;
-        
-        lang = s.substr(0, s.find("."));
-      }
-      rewind(fptr_t);
+      fcm->train(fptr, a);
+      lang_fcm.push_back({fcm, s});
       fclose(fptr);
     }
   }
-  fclose(fptr_t);
+  locatelang(lang_fcm, fptr_t, a, k);
   closedir(dp);
-  printf("%s: %f avg bits\n", lang.c_str(), min_entropy);
+  fclose(fptr_t);
 
   return 0;
 }
